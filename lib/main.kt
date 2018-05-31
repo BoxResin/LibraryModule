3ccb8ffb137efd27bbce2d6afef9f84b640c085a
Minsuk Eom <boxresin@gmail.com>
2018-05-31 11:24:11 +0900

    Deprecated 된 함수 모두 삭제

--- lib/main.kt
+++ lib/main.kt
@@ -1,13 +1,3 @@
-fun max3(a: Int, b: Int, c: Int)
-{
-	return Math.max(a, Math.max(b, c))
-}
-
-fun min3(a: Int, b: Int, c: Int)
-{
-	return Math.min(a, Math.min(b, c))
-}
-
 fun maxN(varargs nums: Int)
 {
 	return nums.max()
